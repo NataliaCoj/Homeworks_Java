81bb943ff23dbfe43e02263d18ebe24fbe2f491d
NataliaCoj <brook.nata@gmail.com>
2020-07-28 11:51:43 +0200

    Register change

---
+++ Register_change/src/RegisterChange.java
@@ -0,0 +1,7 @@
+public class RegisterChange {
+    public static void main(String[] args) {
+        String str = "You won € 5000!";
+        System.out.println(str.toLowerCase()); // you won € 5000!
+        System.out.println(str.toUpperCase()); // YOU WON € 5000!
+    }
+}
